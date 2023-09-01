f355652360af7a28987ff4907e5eb94d980b7193
afzalfaizi <afzaalm993@gmail.com>
2023-09-01 11:29:43 +0500

    task added

---
+++ function.js
@@ -0,0 +1,9 @@
+"use strict";
+// write a ts program that's calculate sum of two numbers using function
+function sumOfTwoNum() {
+    var num1 = 10;
+    var num2 = 20;
+    var result = num1 + num2;
+    console.log("result is=", result);
+}
+sumOfTwoNum();
---
+++ function.ts
@@ -0,0 +1,9 @@
+// write a ts program that's calculate sum of two numbers using function
+function sumOfTwoNum(){
+var num1:number = 10
+var num2:number = 20
+var result:number = num1 + num2
+console.log("result is=", result);
+
+}
+sumOfTwoNum();
